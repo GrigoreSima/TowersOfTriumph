18abf99d460284c4ac2e0e97bf7f6bdc315862bd
Starrlightz <silion.liviu@yahoo.com>
2025-01-13 01:56:00 +0200

    WORK

--- Assets/Scripts/WreckingBallController.cs
+++ Assets/Scripts/WreckingBallController.cs
@@ -1,28 +1,19 @@
 using UnityEngine;
 
 public class WreckingBallController : MonoBehaviour {
-	public HingeJoint _hingeToDestroy;
 	public Rigidbody stopper;
 	private bool destroyed;
-	private bool detached;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start() {
 		destroyed = false;
-		detached = false;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		if (!destroyed && Input.GetKey(KeyCode.Space)) {
+		if (!destroyed && Input.GetKey(KeyCode.X)) {
 			destroyed = true;
 			stopper.isKinematic = false;
-			stopper = null;
-			return;
-		}
-		if(destroyed && !detached && Input.GetKey(KeyCode.X)) {
-			Destroy(_hingeToDestroy);
-			detached = true;
 			return;
 		}
 	}
